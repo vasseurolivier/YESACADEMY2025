e2f2a82588c8ece87a0e77b2d4e58409ca86ed83
olivier vasseur <vasseurolivier86@gmail.com>
2025-10-13 08:33:32 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '..

--- src/app/admin/(main)/photos/upload-form.tsx
+++ src/app/admin/(main)/photos/upload-form.tsx
@@ -1,7 +1,8 @@
+
 'use client';
 
 import { useState, useTransition } from 'react';
-import { handleImageUpload } from "../actions";
+import { handleImageUpload } from "./actions";
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useToast } from '@/hooks/use-toast';
